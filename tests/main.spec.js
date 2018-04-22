c1c15a0ac5581e9694de3ee0e61260ba200ed2d1
Mauricio Vieira <mauricio@mauriciovieira.net>
2018-04-22 17:00:45 +0300

    Test using sinon

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,4 +1,8 @@
-import { expect } from 'chai'
+import chai, { expect } from 'chai'
+import sinon from 'sinon'
+import sinonChai from 'sinon-chai'
+import sinonStubPromise from 'sinon-stub-promise'
+
 import { search,
   searchAlbums,
   searchArtists,
@@ -6,14 +10,12 @@ import { search,
   searchPlaylists,
 } from '../src/main'
 
+chai.use(sinonChai)
+sinonStubPromise(sinon)
+global.fetch = require('node-fetch')
+
 describe('Spotify Wrapper', () => {
   describe('Smoke tests',() => {
-    // search
-    // searchAlbums
-    // searchArtists
-    // searchTracks
-    // searchPlaylists
-
     it('should exist search method', () => {
       expect(search).to.exist;
     })
@@ -28,6 +30,43 @@ describe('Spotify Wrapper', () => {
     })
     it('should exist searchPlaylists method', () => {
       expect(searchPlaylists).to.exist;
+    })
+  })
+
+  describe('Generic Search', () => {
+    let fetchedStub
+    beforeEach(()=>{
+      fetchedStub = sinon.stub(global, 'fetch');
+    })
+    afterEach(()=>{
+      fetchedStub.restore();
+    })
+    it('should call fetch function', () => {
+      const artists = search()
+
+      expect(fetchedStub).to.have.been.calledOnce;
+    })
+
+    it('should receive the correct url to fetch', () => {
+
+      context('passing one type', () => {
+        const artists = search('Incubus', 'artist')
+
+        expect(fetchedStub).to.have.been
+          .calledWith('https://api.spotify.com/v1/search?q=Incubus&type=artist')
+
+        const albums = search('Incubus', 'album')
+        expect(fetchedStub).to.have.been
+        .calledWith('https://api.spotify.com/v1/search?q=Incubus&type=album')
+      })
+
+      context('passing more than one type', () => {
+        const artistsAndAlbums = search('Incubus', ['artist', 'album'])
+
+        expect(fetchedStub).to.have.been
+        .calledWith('https://api.spotify.com/v1/search?q=Incubus&type=artist,album')
+
+      })
     })
   })
 })
