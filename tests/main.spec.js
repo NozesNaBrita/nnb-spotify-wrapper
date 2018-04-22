b38e1b188a417e0f9833dbbb3ad628c67bbfbf1d
Mauricio Vieira <mauricio@mauriciovieira.net>
2018-04-22 16:38:33 +0300

    Add smoke tests

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,7 +1,33 @@
 import { expect } from 'chai'
+import { search,
+  searchAlbums,
+  searchArtists,
+  searchTracks,
+  searchPlaylists,
+} from '../src/main'
 
 describe('Spotify Wrapper', () => {
   describe('Smoke tests',() => {
+    // search
+    // searchAlbums
+    // searchArtists
+    // searchTracks
+    // searchPlaylists
 
+    it('should exist search method', () => {
+      expect(search).to.exist;
+    })
+    it('should exist searchAlbums method', () => {
+      expect(searchAlbums).to.exist;
+    })
+    it('should exist searchArtists method', () => {
+      expect(searchArtists).to.exist;
+    })
+    it('should exist searchTracks method', () => {
+      expect(searchTracks).to.exist;
+    })
+    it('should exist searchPlaylists method', () => {
+      expect(searchPlaylists).to.exist;
+    })
   })
 })
