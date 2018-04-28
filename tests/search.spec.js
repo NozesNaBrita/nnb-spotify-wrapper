c466f6469d8ef6c62d6cf769f498c0ecd8b46a49
Mauricio Vieira <mauricio@mauriciovieira.net>
2018-04-28 15:39:52 +0300

    Rename spec description

--- tests/search.spec.js
+++ tests/search.spec.js
@@ -14,7 +14,7 @@ chai.use(sinonChai)
 sinonStubPromise(sinon)
 global.fetch = require('node-fetch')
 
-describe('Spotify Wrapper', () => {
+describe('Search', () => {
   let fetchedStub
   let promise
 
