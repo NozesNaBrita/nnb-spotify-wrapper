49d0675589e07ba7dc0d557b0ce65158b10fa5aa
Mauricio Vieira <mauricio@mauriciovieira.net>
2018-05-18 21:33:18 +0300

    Remove dotenv

--- src/config.js
+++ src/config.js
@@ -1,8 +1,6 @@
-require('dotenv').config({ path: `${__dirname}/../.env` });
-
-export const API_URL = `${process.env.API_URL}`;
+export const API_URL = 'https://api.spotify.com/v1';
 export const HEADERS = {
   headers: {
-    Authorization: `'Bearer ${process.env.API_TOKEN}'`,
+    Authorization: 'Bearer MYTOKEN',
   },
 };
